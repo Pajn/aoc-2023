7a4ddfe644fe6f1c9b8be6e26c8adcb494183bda
Rasmus Eneman <rasmus@eneman.eu>
2023-12-01 22:17:59 +0100

    day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,25 +1,69 @@
-use nom::IResult;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::anychar,
+    combinator::{iterator, map, map_res, peek, value},
+    multi::many_till,
+    sequence::pair,
+    IResult,
+};
 
 use crate::days::Day;
 
 pub struct Day01;
 
 impl Day for Day01 {
-    type Input = String;
+    type Input = Vec<String>;
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        Ok((
+            input,
+            input.trim().split('\n').map(ToString::to_string).collect(),
+        ))
     }
 
-    type Output1 = usize;
+    type Output1 = u32;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        fn to_digit(input: &str) -> IResult<&str, u32> {
+            map_res(anychar, |c| c.to_digit(10).ok_or(()))(input)
+        }
+
+        calc(input, to_digit)
     }
 
-    type Output2 = usize;
+    type Output2 = u32;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        fn to_digit(input: &str) -> IResult<&str, u32> {
+            alt((
+                map_res(anychar, |c| c.to_digit(10).ok_or(())),
+                value(1, pair(peek(tag("one")), anychar)),
+                value(2, pair(peek(tag("two")), anychar)),
+                value(3, pair(peek(tag("three")), anychar)),
+                value(4, pair(peek(tag("four")), anychar)),
+                value(5, pair(peek(tag("five")), anychar)),
+                value(6, pair(peek(tag("six")), anychar)),
+                value(7, pair(peek(tag("seven")), anychar)),
+                value(8, pair(peek(tag("eight")), anychar)),
+                value(9, pair(peek(tag("nine")), anychar)),
+            ))(input)
+        }
+
+        calc(input, to_digit)
     }
+}
+
+fn calc(input: &Vec<String>, to_digit: fn(&str) -> IResult<&str, u32>) -> u32 {
+    input
+        .iter()
+        .map(AsRef::as_ref)
+        .map(|row| {
+            let mut digits = iterator(row, map(many_till(anychar, to_digit), |(_, v)| v));
+            let mut digits = digits.into_iter();
+            let first = digits.next().expect("first digit");
+            let last = digits.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .sum()
 }
