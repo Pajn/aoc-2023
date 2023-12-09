74365eb2d44410aa6ab6975ded6f89f25adc293f
Rasmus Eneman <rasmus@eneman.eu>
2023-12-09 11:15:26 +0100

    day 9

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,25 +1,85 @@
-use nom::IResult;
+use itertools::Itertools;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{line_ending, multispace0},
+    combinator::all_consuming,
+    multi::separated_list1,
+    sequence::preceded,
+    IResult,
+};
 
-use crate::days::Day;
+use crate::{
+    days::Day,
+    helpers::{parse_digit, parse_ndigit},
+};
 
 pub struct Day09;
 
 impl Day for Day09 {
-    type Input = String;
+    type Input = Vec<Vec<isize>>;
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        // let input = "0 3 6 9 12 15
+        // 1 3 6 10 15 21
+        // 10 13 16 21 30 45";
+
+        all_consuming(separated_list1(
+            preceded(line_ending, multispace0),
+            separated_list1(
+                tag(" "),
+                alt((parse_digit("num"), parse_ndigit("negative num"))),
+            ),
+        ))(input)
     }
 
-    type Output1 = usize;
+    type Output1 = isize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        input.into_iter().map(extrapolate_end).sum()
     }
 
-    type Output2 = usize;
+    type Output2 = isize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        input.into_iter().map(extrapolate_start).sum()
     }
+}
+
+fn extrapolate_end(row: &Vec<isize>) -> isize {
+    let differences = calculate_differences(&row);
+
+    let next_difference = if differences.iter().all(|n| *n == 0) {
+        0
+    } else {
+        extrapolate_end(&differences)
+    };
+
+    row.last().unwrap() + next_difference
+}
+
+fn extrapolate_start(row: &Vec<isize>) -> isize {
+    let differences = calculate_differences(&row);
+
+    let previous_difference = if differences.iter().all(|n| *n == 0) {
+        0
+    } else {
+        extrapolate_start(&differences)
+    };
+
+    row.first().unwrap() - previous_difference
+}
+
+fn calculate_differences(row: &Vec<isize>) -> Vec<isize> {
+    let mut row_iter = row.iter();
+    let first = row_iter.next().copied();
+    row_iter
+        .scan(first, |prev, curr| {
+            prev.as_mut().map(|prev| {
+                let val = *curr - *prev;
+                *prev = *curr;
+                val
+            })
+        })
+        .collect_vec()
 }
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,9 +1,11 @@
-use std::str::FromStr;
 use std::{fmt::Debug, ops::RangeFrom};
+use std::{ops::RangeTo, str::FromStr};
 
 use nom::{
-    character::complete::digit1, combinator::map, AsChar, IResult, InputIter, InputTakeAtPosition,
-    ParseTo, Slice,
+    character::complete::{char, digit1},
+    combinator::{map, recognize},
+    sequence::preceded,
+    AsChar, IResult, InputIter, InputTakeAtPosition, Offset, ParseTo, Slice,
 };
 
 pub fn parse_digit<I, T>(error_message: &'static str) -> impl FnMut(I) -> IResult<I, T>
@@ -15,4 +17,22 @@ where
     T::Err: Debug,
 {
     map(digit1, |n: I| n.parse_to().expect(error_message))
+}
+pub fn parse_ndigit<I, T>(error_message: &'static str) -> impl FnMut(I) -> IResult<I, T>
+where
+    I: Slice<RangeFrom<usize>>
+        + InputIter
+        + ParseTo<T>
+        + InputTakeAtPosition
+        + Clone
+        + Offset
+        + Slice<RangeTo<usize>>,
+    <I as InputIter>::Item: AsChar,
+    <I as InputTakeAtPosition>::Item: AsChar,
+    T: FromStr,
+    T::Err: Debug,
+{
+    map(recognize(preceded(char('-'), digit1)), |n: I| {
+        n.parse_to().expect(error_message)
+    })
 }
